60a54d0f5dc2aa5cfa803548048e9c32d9a963fe
IlyaGoncharovY <goncharov.i@rambler.ru>
2021-12-29 00:41:45 +0300

    test Ok

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function incrementer(nums) { 
-    // Given an input of an array of digits, 
-    // return the array with each digit incremented by 
-    // its position in the array: the first digit will 
-    // be incremented by 1, the second digit by 2, etc. 
-    // Make sure to start counting your positions from 1 (and not 0).
-
-    // Your result can only contain single digit numbers, 
-    // so if adding a digit with it's position gives you 
-    // a multiple-digit number, only the last digit of the 
-    // number should be returned.
-   }
+function incrementer(nums) {
+    let arr = nums.map((n, i) => (n + i + 1) % 10);
+    return arr;
+}
